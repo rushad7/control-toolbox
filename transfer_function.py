955e971aa04ff1692f593030270f3b8fad159cbc
Rushad Mehta <rushadmehta16@gmail.com>
2020-05-30 01:00:24 +0530

    Updated display method

--- transfer_function.py
+++ transfer_function.py
@@ -33,36 +33,44 @@ class TransferFunction():
         self.num_str = ""
         for n in range(len(self.num_coef)):
             if n < len(self.num_coef)-1:
-                if self.num_coef[n] != 1:
-                    self.num_str = self.num_str + str(int(self.num_coef[n])) + "*S^" + str(abs(n-len(self.num_coef)+1)) + " + "
-                elif self.num_coef[n] == 1:
-                    self.num_str = self.num_str + "S^" + str(abs(n-len(self.num_coef)+1)) + " + "
-                elif self.num_coef[n] == 0:
-                    pass
+                if n != len(self.num_coef)-2:
+                    if self.num_coef[n] != 1:
+                        self.num_str = self.num_str + str(int(self.num_coef[n])) + "*S^" + str(abs(n-len(self.num_coef)+1)) + " + "
+                    elif self.num_coef[n] == 1:
+                        self.num_str = self.num_str + "S^" + str(abs(n-len(self.num_coef)+1)) + " + "
+                    elif self.num_coef[n] == 0:
+                        pass
+                else:
+                    if self.num_coef[n] != 1:
+                        self.num_str = self.num_str + str(int(self.num_coef[n])) + "*S" + " + "
+                    else:
+                        self.num_str = self.num_str  + "S" + " + "
             else:
-                if self.num_coef[n] != 1:
+                if self.num_coef[n] != 0:
                     self.num_str = self.num_str + str(int(self.num_coef[n]))
-                elif self.num_coef[n] == 1:
-                    self.num_str = self.num_str + "S^"
                 elif self.num_coef[n] == 0:
-                    pass
+                    self.num_str = self.num_str[:-3]
                   
         self.den_str = ""
         for d in range(len(self.den_coef)):
             if d < len(self.den_coef)-1:
-                if self.den_coef[d] != 1:
-                    self.den_str = self.den_str + str(int(self.den_coef[d])) + "*S^" + str(abs(d-len(self.den_coef)+1)) + " + "
-                elif self.den_coef[d] == 1:
-                    self.den_str = self.den_str + "S^"
-                elif self.den_coef[d] == 0:
-                    pass
+                if d != len(self.den_coef)-2:
+                    if self.den_coef[d] != 1:
+                        self.den_str = self.den_str + str(int(self.den_coef[d])) + "*S^" + str(abs(d-len(self.den_coef)+1)) + " + "
+                    elif self.den_coef[d] == 1:
+                        self.den_str = self.den_str + "S^" + str(abs(d-len(self.den_coef)+1)) + " + "
+                    elif self.den_coef[d] == 0:
+                        pass
+                else:
+                    if self.den_coef[d] != 1:
+                        self.den_str = self.den_str + str(int(self.den_coef[d])) + "*S" + " + "
+                    else:
+                        self.den_str = self.den_str  + "S" + " + "
             else:
-                if self.den_coef[d] != 1:
-                    self.den_str = self.den_str + str(int(self.num_coef[n]))
-                elif self.den_coef[d] == 1:
-                    self.den_str = self.den_str + "S^" + str(abs(d-len(self.den_coef)+1))
+                if self.den_coef[d] != 0:
+                    self.den_str = self.den_str + str(int(self.den_coef[d]))
                 elif self.den_coef[d] == 0:
-                    pass
+                    self.den_str = self.den_str[:-3]
         
         self.div_line_len = max(len(self.num_str), len(self.den_str))
         self.div_line = self.div_line_len*"-"
