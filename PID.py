4f7873af5e3282177fc519dd8a968c9b420d4d1f
Rushad Mehta <rushadmehta16@gmail.com>
2020-05-29 18:53:32 +0530

    Added PID class

--- PID.py
+++ PID.py
@@ -6,11 +6,37 @@ Created on Fri May 29 14:28:21 2020
 """
 import numpy as np
 
-def def_pid(K_p, K_i, K_d, transfer_function, input_type, time_period=3):
+class PID():
     
-    pid_controller = {"kp":K_p, "ki":K_i, "kd":K_d}
-    #inputs = {"impulse" : "[1 if i == 0 else 0 for i in range(5)]", "step" : "[1 for i in range(4)]", "ramp" : " [i for i in range(4)]"}
-    inputs = {"impulse" : "[1 if i == 0 else 0 for i in np.arange(0, time_period, 0.2)]", "step" : "[1 for i in np.arange(0, time_period, 0.2)]", "ramp" : " [i for i in np.arange(0, time_period, 0.2)]"} 
-    time = eval(inputs[input_type])
+    def __init__(self, K_p, K_i, K_d, transfer_function, input_type, time_period=3):
+        
+        self.K_p = K_p
+        self.K_i = K_i
+        self.K_d = K_d
+        self.transfer_function = transfer_function
+        self.input_type = input_type
+        self.time_period = time_period
+        self.inputs = {"impulse" : "[1 if i == 0 else 0 for i in np.arange(0, self.time_period, 0.2)]", "step" : "[1 for i in np.arange(0, self.time_period, 0.2)]", "ramp" : " [i for i in np.arange(0, self.time_period, 0.2)]"}
+        
+    def init(self):        
+        pid_controller = {"kp":self.K_p, "ki":self.K_i, "kd":self.K_d}
+        self.time = eval(self.inputs[self.input_type])
+        return pid_controller
+    
+    def display(self):
+        '''
+        Displays the PID TF block
+
+        '''
+        pid_tf = self.init()
+        
+        self.num_str = str(self.K_d) + " *S^2 + " + str(self.K_p) + " *S + " + str(self.K_i) 
+        self.den_str = "S"
+        
+        self.div_line_len = max(len(self.num_str), len(self.den_str))
+        self.div_line = self.div_line_len*"-"
+        pid_tf_disp = str(self.num_str + " \n" + self.div_line + " \n" + self.den_str)
+        print(pid_tf_disp)
+        
     
     
