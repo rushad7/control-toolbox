b61488846884d77071c326bba90272cb9a3ff15a
Rushad Mehta <rushadmehta16@gmail.com>
2020-05-30 01:43:13 +0530

    Further improved disp method and added comments

--- transfer_function.py
+++ transfer_function.py
@@ -27,49 +27,54 @@ class TransferFunction():
     def display(self):
         '''
         Displays TF block
-
+        
         '''
         
         self.num_str = ""
         for n in range(len(self.num_coef)):
-            if n < len(self.num_coef)-1:
-                if n != len(self.num_coef)-2:
-                    if self.num_coef[n] != 1 and self.num_coef[n] != 0:
+            if n < len(self.num_coef)-1: #if not last
+                if n != len(self.num_coef)-2: #if not second last
+                    if self.num_coef[n] != 1 and self.num_coef[n] != 0: #if coef is not zero and one
                         self.num_str = self.num_str + str(int(self.num_coef[n])) + "*S^" + str(abs(n-len(self.num_coef)+1)) + " + "
-                    elif self.num_coef[n] == 1:
+                    elif self.num_coef[n] == 1: #if coef is one
                         self.num_str = self.num_str + "S^" + str(abs(n-len(self.num_coef)+1)) + " + "
-                    elif self.num_coef[n] == 0:
+                    elif self.num_coef[n] == 0: #if coef is zero
                         pass
-                else:
-                    if self.num_coef[n] != 1:
+                else: #if second last
+                    if self.num_coef[n] != 1 and self.num_coef[n] != 0: #if coef is not zero and one
                         self.num_str = self.num_str + str(int(self.num_coef[n])) + "*S" + " + "
-                    else:
+                    elif self.num_coef[n] == 1: #if coef is one
                         self.num_str = self.num_str  + "S" + " + "
-            else:
-                if self.num_coef[n] != 0:
+                    elif self.num_coef[n] == 0: #if coef is zero
+                        pass
+                        
+            else: #if last
+                if self.num_coef[n] != 0: #if coef is not zero
                     self.num_str = self.num_str + str(int(self.num_coef[n]))
-                elif self.num_coef[n] == 0:
+                elif self.num_coef[n] == 0: #if coef is zero
                     self.num_str = self.num_str[:-3]
                   
         self.den_str = ""
         for d in range(len(self.den_coef)):
-            if d < len(self.den_coef)-1:
-                if d != len(self.den_coef)-2:
-                    if self.den_coef[d] != 1 and self.den_coef[d] != 0:
+            if d < len(self.den_coef)-1: #if not last 
+                if d != len(self.den_coef)-2: #if not second last
+                    if self.den_coef[d] != 1 and self.den_coef[d] != 0: #if coef not zero and one
                         self.den_str = self.den_str + str(int(self.den_coef[d])) + "*S^" + str(abs(d-len(self.den_coef)+1)) + " + "
-                    elif self.den_coef[d] == 1:
+                    elif self.den_coef[d] == 1: #if coef is one
                         self.den_str = self.den_str + "S^" + str(abs(d-len(self.den_coef)+1)) + " + "
-                    elif self.den_coef[d] == 0:
+                    elif self.den_coef[d] == 0: #if coef is zer0
                         pass
-                else:
-                    if self.den_coef[d] != 1:
+                else: #if second last
+                    if self.den_coef[d] != 1 and self.den_coef[d] != 0: #if coef is not zero and one
                         self.den_str = self.den_str + str(int(self.den_coef[d])) + "*S" + " + "
-                    else:
+                    elif self.den_coef[d] == 1: #if coef is one
                         self.den_str = self.den_str  + "S" + " + "
-            else:
-                if self.den_coef[d] != 0:
+                    elif self.den_coef[d] == 0: #if coef is zero
+                        pass
+            else: #if last
+                if self.den_coef[d] != 0: #if coef is not zero
                     self.den_str = self.den_str + str(int(self.den_coef[d]))
-                elif self.den_coef[d] == 0:
+                elif self.den_coef[d] == 0: #if coef is zero
                     self.den_str = self.den_str[:-3]
         
         self.div_line_len = max(len(self.num_str), len(self.den_str))
