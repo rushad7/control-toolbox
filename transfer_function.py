742156db2c426463182f7155455fab60f242943b
Rushad Mehta <rushadmehta16@gmail.com>
2020-05-31 01:57:34 +0530

    Addded second order impulse response

--- transfer_function.py
+++ transfer_function.py
@@ -18,8 +18,8 @@ class TransferFunction():
         self.den_coef = den_coef
         self.num_coef = self.num_coef.reshape([len(self.num_coef), 1])
         self.den_coef = self.den_coef.reshape([len(self.den_coef), 1])
-        if (max(len(self.num_coef), len(self.den_coef)) > 2):
-            print("[WARNING] You have inputed a system of Order:" + str(max(len(self.num_coef), len(self.den_coef))) + "\n" + "Current support is for first and second order systems\n")
+        if (max(len(self.num_coef), len(self.den_coef))-1 > 2):
+            print("[WARNING] You have inputed a system of Order:" + str(max(len(self.num_coef), len(self.den_coef))-1) + "\n" + "Current support is for first and second order systems\n")
             print("Continuing WILL NOT produce correct results")
     def init(self):
         '''
@@ -95,7 +95,7 @@ class TransferFunction():
         self.time_period = time_period
         self.sample_time = sample_time
         self.controller_time = np.array([i for i in np.arange(0, self.time_period, self.sample_time)])
-        self.input_resp = {"impulse":"impulse_order1(self)", "step":"step_order1(self)", "ramp":"ramp_order1(self)"}
+        self.input_resp = {"impulse":"impulse_order2(self)", "step":"step_order1(self)", "ramp":"ramp_order1(self)"}
         
         def impulse_order1(self):
             resp = (float(self.num_coef[0])/float(self.den_coef[0]))*np.exp(-self.controller_time/float(self.den_coef[0]))
@@ -109,6 +109,17 @@ class TransferFunction():
             resp = float(self.num_coef[0])*(float(-self.den_coef[0]) + self.controller_time + np.exp(-self.controller_time/float(self.den_coef[0])))
             return resp
         
+        def impulse_order2(self):
+            self.natural_frequency = float(np.sqrt(self.den_coef[2]))
+            self.damping_ratio = self.den_coef[1]/(2*self.natural_frequency)
+            
+            if self.damping_ratio > 1: 
+                resp = (self.natural_frequency/(np.sqrt(self.damping_ratio**2 - 1)))*np.exp(-self.damping_ratio*self.natural_frequency*self.controller_time)*np.sinh(self.natural_frequency*np.sqrt(self.damping_ratio**2 - 1))*self.controller_time
+            elif self.damping_ratio < 1:
+                resp = (self.natural_frequency/(np.sqrt(1 - self.damping_ratio**2)))*np.exp(-self.damping_ratio*self.natural_frequency*self.controller_time)*np.sin(self.natural_frequency*np.sqrt(1- self.damping_ratio**2))*self.controller_time                
+            
+            return resp
+            
         resp = eval(self.input_resp[self.input_type])
         
         plt.plot(self.controller_time, resp)
