9e75fcdbadc97303147ff30cf5a8a10f683c0466
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-06 23:15:15 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='control-system',
+    packages=['control'],
+    version='1.0.0',
+    description='Control Systems Toolbox for Python',
+    url='https://github.com/rushad7/control-toolbox',    
+)
