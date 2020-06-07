ee4641074c11d8373a2d4bd21946fb3baeed1383
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-07 17:39:08 +0530

    Removed redundant variables Added docstrings

--- control/rootlocus.py
+++ control/rootlocus.py
@@ -4,14 +4,26 @@ Created on Sun Jun  7 16:20:26 2020
 
 @author: Rushad
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rootlocus(tf, gain_range=10.0):
+    '''
+    Parameters
+    ----------
+    tf : Transfer Function object
+        DESCRIPTION. TF object whose root locus we want to find
+    gain_range : float / int, optional
+        DESCRIPTION. Range of the gain. The default is 10.0.
 
+    Returns
+    -------
+    Root Locus plot of the system
 
-def rootlocus(tf):
+    '''
     
-    gains = np.linspace(0.0, 20.0, num=500)
+    gains = np.linspace(0.0, float(gain_range), num=500)
     
     num = tf.num_coef
     num = list(tf.num_coef.reshape(len(num),))    
@@ -21,21 +33,9 @@ def rootlocus(tf):
     temp = np.zeros(size)
     num = np.concatenate((temp, num))
     tf = np.vstack((num, den))
-    print(tf)
     
     def compute_roots(tf, gains):
-      """Computes the roots of the characteristic equation of the closed-loop system
-      of a given transfer function for a list of gain parameters.
-      Concretely, given TF = zeros/poles, and a gain value K, we solve for the
-      characteristic equation roots, that is the roots of poles + (K * zeros).
-      Args:
-        tf: transfer function.
-        gains: list of gains.
-      Returns:
-        roots: numpy array containing the roots for each gain in gains.
-      """
       num, den = tf[0], tf[1]
-      num_roots = len(np.roots(den))
       roots = []
     
       for gain in gains:
@@ -51,16 +51,12 @@ def rootlocus(tf):
 
 
     def plot_root_locus(gains, roots):
-      """Plots the root locus of the closed loop system given the provided gains.
-      """
-      # get real and imaginary values
+        
       real_vals = np.real(roots)
       imag_vals = np.imag(roots)
     
-      # possible colors
       colors = ['b', 'm', 'c', 'r', 'g']
     
-      # create figure and axis labels
       fig, ax = plt.subplots()
       ax.set_xlabel('Re')
       ax.set_ylabel('Im')
@@ -73,11 +69,7 @@ def rootlocus(tf):
               color='blue')
     
       # plots a red "o" for the last roots
-      ax.scatter(real_vals[-1, :], imag_vals[-1, :],
-              marker='o',
-              color='red')
-    
-      gain_text = ['k = {:1.2f}'.format(k) for k in gains]
+      ax.scatter(real_vals[-1, :], imag_vals[-1, :], marker='o', color='red')
     
       temp_real_vals = real_vals[1:-1, :]
       temp_imag_vals = imag_vals[1:-1, :]
