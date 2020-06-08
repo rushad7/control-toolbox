2c550f3f64f5d27253a462a4f0f0c61cdc25957c
Rushad Mehta <54734306+rushad7@users.noreply.github.com>
2020-06-08 16:51:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup, find_packages
-
-setup(name='control-toolbox',
-      version='0.0.1',
-      description='Control Systems Toolbox for Python',
-      author='Rushad Mehta',
-      author_email='rushadmehta16@gmail.com',
-      url='https://github.com/rushad7/control-toolbox',
-      packages=find_packages(),
-     )
+from distutils.core import setup, find_packages
+setup(
+  name = 'control-toolbox',
+  packages = find_packages(),
+  version = '0.1', 
+  license='MIT',
+  description = 'Python Control System Toolbox',
+  author = 'Rushad Mehta',
+  author_email = 'rushadmehta16@gmail.com',
+  url = 'https://github.com/rushad7/control-toolbox',
+  download_url = 'https://github.com/rushad7/control-toolbox/archive/v0.1.tar.gz'
+  keywords = ['Control Systems', 'Python Control Toolbox', 'System Simulation'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5'
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
