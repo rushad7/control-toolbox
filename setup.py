b33136b32aab28b285a9b1be32b7dbbfa91c7882
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-10 15:30:21 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fp:
 setup(
   name = 'control-toolbox',
   packages = ['control'],
-  version = '0.0.1', 
+  version = '0.0.2', 
   license='MIT',
   description = 'Python Control System Toolbox',
   long_description=long_description,
