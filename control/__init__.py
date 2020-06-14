468a16dd758bfa36648e99d60d9a81bf2ec0d005
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-14 17:53:14 +0530

    Updated __init__.py

--- control/__init__.py
+++ control/__init__.py
@@ -1,2 +1,3 @@
-from control.system import TransferFunction, feedback, PID
+from control.system import TransferFunction, feedback, PID, reduce, StateSpace
 from control.rootlocus import rootlocus
+from control.bode import bode
