64ae86a5623d6448cd1a245eb71ac17e563f039d
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-25 01:12:09 +0530

    Added LQR class to __init__

--- control/__init__.py
+++ control/__init__.py
@@ -2,3 +2,4 @@ from .system import TransferFunction, feedback, PID, reduce, StateSpace
 from .rootlocus import rootlocus
 from .bode import bode
 from .StateFeedback import StateFeedback
+from .LQR import LQR
