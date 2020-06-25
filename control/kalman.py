d558cbee93066f260e3cf345792371bc67875123
Rushad Mehta <rushadmehta16@gmail.com>
2020-06-26 00:46:49 +0530

    Modified KalmanFilter class

--- control/kalman.py
+++ control/kalman.py
@@ -6,59 +6,59 @@ Created on Thu Jun 25 19:01:57 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class KalmanFilter():
     
-    def __init__(self, ss, Q = None, R = None, P = None, x0 = None):
+    def __init__(self, ss, Q, R, P=None, x0=None):
 
         self._ss = ss
         self._A = self._ss.A
         self._B = self._ss.B
         self._C = self._ss.C
 
-        if(self._A is None or self._C is None):
-            raise ValueError("Set proper system dynamics.")
-
         self._n = self._A.shape[1]
         self._m = self._C.shape[1]
 
-        self.Q = np.eye(self._n) if Q is None else Q
-        self.R = np.eye(self._n) if R is None else R
+        self.Q = Q
+        self.R = R
         self.P = np.eye(self._n) if P is None else P
-        self.x = np.zeros((self.n, 1)) if x0 is None else x0
+        self.x = np.zeros((self._n, 1)) if x0 is None else x0
 
     def predict(self, u = 0):
         
-        self.x = self._A@self.x + self._B@u
-        self.P = self._A@self.P@self._A.T + self.Q
+        self.x = np.dot(self._A, self.x) + np.dot(self._B, u)
+        self.P = np.dot(np.dot(self._A, self.P), self._A.T) + self.Q
         
         return self.x
 
     def update(self, z):
         
-        inv = np.linalg.inv(self.R + self._C@self.P@self._C.T)
-        K = self.P@self._C.T@inv
+        inv = np.linalg.inv(self.R + np.dot(self._C, np.dot(self.P, self._C.T)))
+        K = np.dot(np.dot(self.P, self._C.T), inv)
         
-        y = z - self._C@self.x
-        self.x = self.x + K@y
+        y = z - np.dot(self._C, self.x)
+        self.x = self.x + np.dot(K, y)
         
         I = np.eye(self._n)
-        self.P = (I - K@self._C)@self.P@((I - K@self._C).T) + K@self.R@K.T
+        self.P = np.dot(np.dot(I - np.dot(K, self._C), self.P), (I - np.dot(K, self._C)).T) + np.dot(np.dot(K, self.R), K.T)
+        
+        y = np.dot(self._C,self.x)
+        return y
 
     def solve(self, measurements):
         
-        m_upd = np.zeros(shape=measurements.shape)
         time = np.array(range(len(measurements)))
-        predictions = []
-    
-        for z in m_upd:
-            predictions.append(self._ss.C@self.predict())[0]
-            self.update(z)
-    
-        import matplotlib.pyplot as plt
-        plt.plot(time, m_upd, label = 'Measurements')
-        plt.plot(time, np.array(predictions), label = 'Kalman Filter Prediction')
-        plt.plot(time, measurements, label = 'Ideal')
+        state_predictions = []
+        output_predictions = []
+        
+        for z in measurements:
+            state_predictions.append(np.dot(self._C,  self.predict())[0])
+            output_predictions.append(self.update(z)[0][0])
+        
+        plt.plot(time, np.array(output_predictions), label = 'Kalman Filter Prediction')
+        plt.plot(time, measurements, label = 'Measurements')
         plt.legend()
         plt.show()
-        return measurements
+        
+        return output_predictions
