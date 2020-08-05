ebd1a7758340be1a14e1fb584367b7db5673c8ba
Rushad Mehta <rushadmehta16@gmail.com>
2020-08-05 13:41:49 +0530

    Removed redundant imports. Added fit() and model() methods.

--- control/SystemIdentification.py
+++ control/SystemIdentification.py
@@ -5,34 +5,56 @@ Created on Sun Aug  2 16:01:30 2020
 @author: Rushad
 """
 
-import pandas as pd
 import numpy as np
-import tensorflow
+import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dropout, Dense
-from tensorflow.keras.layers import Flatten, LSTM
-from tensorflow.keras.layers import GlobalMaxPooling1D
-from tensorflow.keras.models import Model
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import Dense
 
 class SystemIdentification():
     
-    def __init__(self, path_to_csv):
+    def __init__(self, path_x, path_u, path_x_dot, path_y):
         
-        data = pd.read_csv(path_to_csv)
-        self._x = data[["x", "u"]].to_numpy()
-        self._x_dot = data[["x_dot"]].to_numpy()
-        self._y = data[["y"]].to_numpy()
+        data_x = pd.read_csv(path_x)
+        data_x_dot = pd.read_csv(path_x_dot)
+        data_u = pd.read_csv(path_u)
+        data_y = pd.read_csv(path_y)
         
-    def solve(self):
+        self._x = (data_x.join(data_u)).to_numpy()
+        self._x_dot = data_x_dot.to_numpy()
+        self._y = data_y.to_numpy()
         
-        model = Sequential()
-        model.add(Dense(12, input_dim=2, activation='relu'))
-        model.add(Dense(8, activation='sigmoid'))
-        model.add(Dense(1, activation='relu'))
-        model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-        print(model.summary())
-        model.fit(self._x, self._x_dot, epochs=150, batch_size=10)
+    def fit(self, num_epochs=500):
+        
+        self._model = Sequential()
+        self._model.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid'))
+        self._model.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
+        self._model.add(Dense(2, activation='relu'))
+        self._model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        self._model.fit(self._x, self._x_dot, epochs=num_epochs, batch_size=10)
+        
+    def model(self):
+        
+        def sigmoid(z):
+            a = 1 / (1 + np.exp(-z))
+            return a
+        
+        num_layers = len(self._model.layers)
+        num_neurons = [12, 8, 1]
+        activation_dict = {}
+        prev_op = self._x
+        
+        for l in range(num_layers):
+            
+            try:
+                z = self._model.layers[l].get_weights()[0].T@prev_op.T
+                a = sigmoid(z)
+                activation_dict["a"+str(l+1)] = a
+                prev_op = a
+            
+            except ValueError:
+                z = self._model.layers[l].get_weights()[0].T@prev_op
+                a = sigmoid(z)
+                activation_dict["a"+str(l+1)] = a
+                prev_op = a
+                
+        return activation_dict
