22b6412b82acae578b0ba4f50fdaad6ebd6e45cd
Rushad Mehta <rushadmehta16@gmail.com>
2020-08-07 14:54:56 +0530

    Fixed model() method to return the dict of A and C matrices.

--- control/SystemIdentification.py
+++ control/SystemIdentification.py
@@ -8,7 +8,6 @@ Created on Sun Aug  2 16:01:30 2020
 import warnings
 import numpy as np
 import pandas as pd
-from .system import StateSpace
 from tensorflow.keras import initializers
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
@@ -17,28 +16,27 @@ warnings.filterwarnings("ignore")
 
 class SystemIdentification():
     
-    def __init__(self, path_x, path_u, path_x_dot, path_y):
+    def __init__(self, path_x, path_x_dot, path_y):
         
         data_x = pd.read_csv(path_x)
         data_x_dot = pd.read_csv(path_x_dot)
-        data_u = pd.read_csv(path_u)
         data_y = pd.read_csv(path_y)
         
-        self._x = (data_x.join(data_u)).to_numpy()
+        self._x = data_x.to_numpy()
         self._x_dot = data_x_dot.to_numpy()
         self._y = data_y.to_numpy()
         
     def fit(self, num_epochs=500):
         
         self._stateModel = Sequential()
-        self._stateModel.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
+        self._stateModel.add(Dense(self._x.shape[0], input_dim=2, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
         self._stateModel.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
         self._stateModel.add(Dense(2, activation='relu'))
         self._stateModel.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
         self._stateModel.fit(self._x, self._x_dot, epochs=num_epochs, batch_size=10)
         
         self._outputModel = Sequential()
-        self._outputModel.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
+        self._outputModel.add(Dense(self._x.shape[0], input_dim=2, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
         self._outputModel.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
         self._outputModel.add(Dense(1, activation='relu'))
         self._outputModel.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
@@ -48,12 +46,10 @@ class SystemIdentification():
         
         hyp_state = self._stateModel.predict(self._x)
         hyp_output = self._stateModel.predict(self._x)
-        print(hyp_output.shape)
-        A = print(np.linalg.pinv(self._x).shape)#@hyp_state
-        C = print(np.linalg.pinv(self._x).shape)
         
-        B = D = 0
+        A = np.linalg.pinv(self._x)@hyp_state
+        C = hyp_output.T@np.linalg.pinv(self._x).T
         
-        ss_model = StateSpace(A, B, C, D)
+        model_dict = {"A":A, "C":C}
         
-        return ss_model
+        return model_dict
