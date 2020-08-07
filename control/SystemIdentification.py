633efc809cfdbf2fd3a737cc763053c61460b4fc
Rushad Mehta <rushadmehta16@gmail.com>
2020-08-07 18:10:29 +0530

    Addded docstings.

--- control/SystemIdentification.py
+++ control/SystemIdentification.py
@@ -15,9 +15,25 @@ from tensorflow.keras.models import Sequential
 warnings.filterwarnings("ignore")
 
 class SystemIdentification():
-    
+    '''
+    System Identification module
+    '''
     def __init__(self, path_x, path_x_dot, path_y):
-        
+        '''
+        Parameters
+        ----------
+        path_x : file path
+            DESCRIPTION. path to CSV file consisting the X matrix data. 
+        path_x_dot : file path
+            DESCRIPTION. path to CSV file consisting X_dot matrix data.
+        path_y : file path
+            DESCRIPTION. path to CSV file consisting Y matrix data.
+
+        Returns
+        -------
+        None.
+
+        '''
         data_x = pd.read_csv(path_x)
         data_x_dot = pd.read_csv(path_x_dot)
         data_y = pd.read_csv(path_y)
@@ -27,7 +43,17 @@ class SystemIdentification():
         self._y = data_y.to_numpy()
         
     def fit(self, num_epochs=500):
-        
+        '''
+        Parameters
+        ----------
+        num_epochs : int, optional
+            DESCRIPTION. Number of epochs. The default is 500.
+
+        Returns
+        -------
+        None.
+
+        '''
         self._stateModel = Sequential()
         self._stateModel.add(Dense(self._x.shape[0], input_dim=2, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
         self._stateModel.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
@@ -43,7 +69,13 @@ class SystemIdentification():
         self._outputModel.fit(self._x, self._y, epochs=num_epochs, batch_size=10)
         
     def model(self):
-        
+        '''
+        Returns
+        -------
+        model_dict : dict
+            DESCRIPTION. dictionary of matrices A and C.
+
+        '''
         hyp_state = self._stateModel.predict(self._x)
         hyp_output = self._stateModel.predict(self._x)
         
