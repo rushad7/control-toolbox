c0700dc1f2e5fb0b1b49808e08164d615787b6ac
Rushad Mehta <rushadmehta16@gmail.com>
2020-08-07 14:01:18 +0530

    Modified model() method to compute hyp_state (x_dot) and hyp_output (y). Added outputModel attribute to compute the estimate of the output equation of the state space model (Y = CX).

--- control/SystemIdentification.py
+++ control/SystemIdentification.py
@@ -5,11 +5,15 @@ Created on Sun Aug  2 16:01:30 2020
 @author: Rushad
 """
 
+import warnings
 import numpy as np
 import pandas as pd
-from sympy import symbols, Matrix
+from .system import StateSpace
+from tensorflow.keras import initializers
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
+
+warnings.filterwarnings("ignore")
 
 class SystemIdentification():
     
@@ -26,53 +30,30 @@ class SystemIdentification():
         
     def fit(self, num_epochs=500):
         
-        self._model = Sequential()
-        self._model.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid'))
-        self._model.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
-        self._model.add(Dense(2, activation='relu'))
-        self._model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-        self._model.fit(self._x, self._x_dot, epochs=num_epochs, batch_size=10)
+        self._stateModel = Sequential()
+        self._stateModel.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
+        self._stateModel.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
+        self._stateModel.add(Dense(2, activation='relu'))
+        self._stateModel.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        self._stateModel.fit(self._x, self._x_dot, epochs=num_epochs, batch_size=10)
+        
+        self._outputModel = Sequential()
+        self._outputModel.add(Dense(self._x.shape[0], input_dim=3, activation='sigmoid', kernel_initializer=initializers.glorot_normal()))
+        self._outputModel.add(Dense(self._x.shape[0]/2, activation='sigmoid'))
+        self._outputModel.add(Dense(1, activation='relu'))
+        self._outputModel.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        self._outputModel.fit(self._x, self._y, epochs=num_epochs, batch_size=10)
         
     def model(self):
         
-        def exp(x):
+        hyp_state = self._stateModel.predict(self._x)
+        hyp_output = self._stateModel.predict(self._x)
+        print(hyp_output.shape)
+        A = print(np.linalg.pinv(self._x).shape)#@hyp_state
+        C = print(np.linalg.pinv(self._x).shape)
         
-            expo = Matrix.zeros(x.shape[0], x.shape[1])    
-            for i in range(x.shape[0]):
-                for j in range(x.shape[1]):
-                    expo[i+j] = np.e**(expo[i+j])
+        B = D = 0
         
-            return expo
+        ss_model = StateSpace(A, B, C, D)
         
-        def sigmoid(z):
-            
-            exp_z = exp(-z)
-            for i in range(exp_z.shape[0]*exp_z.shape[1]):
-                exp_z[i] = exp_z[i] + 1
-            
-            for i in range(exp_z.shape[0]*exp_z.shape[1]):
-                exp_z[i] = 1/exp_z[i]
-            
-            return exp_z
-        
-        x_str = ""
-        for i in range(len(self._x[0])-1):
-            x_str = x_str + "x" + str(i+1) + ","
-            
-        x_str = x_str + "u"
-        x_var = symbols(x_str)
-        self.x_matrix = Matrix([x_var]).transpose()
-        x_matrix = Matrix([x_var]).transpose()
-        
-        for l in range(len(self._model.layers)):
-            try:
-                z = Matrix(Matrix(self._model.layers[0].get_weights()[0]).transpose().dot(x_matrix)) + Matrix(self._model.layers[0].get_weights()[1])
-                a = sigmoid(z)
-                x_matrix = a
-            
-            except ValueError:
-                z = Matrix(Matrix(self._model.layers[0].get_weights()[0]).transpose().dot(x_matrix)) + Matrix(self._model.layers[0].get_weights()[1]).T
-                a = sigmoid(z)
-                x_matrix = a                
-            
-        return a
+        return ss_model
